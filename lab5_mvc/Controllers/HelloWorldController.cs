30a2caffc3aff95d5b7aafb31af5ddb87c021a42
willyuan <sevenlevenkimo@gmail.com>
2022-12-08 16:29:44 +0800

    day 2 finish

---
+++ lab5_mvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace lab5_mvc.Controllers;
+public class HelloWorldController:Controller{
+    public IActionResult Index() {
+        return View();
+    }
+    public string Welcome() {
+        return "會接在後面的Welcome";
+    }
+}
