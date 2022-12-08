a32c7af78819304da3b27a1960791c5d0fcdf0fe
willyuan <sevenlevenkimo@gmail.com>
2022-12-08 14:59:35 +0800

    使用ef core

--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Model;
+using var db = new BlogContext();
+Console.WriteLine($"資料庫的路徑是:{db.DbPath}");
+
+db.Add(new Blog { Uri = "http://www.uuu.com.tw/" });
+db.Add(new Blog { Uri = "http://www.tibame.com/" });
+db.Add(new Blog { Uri = "http://hahow.in/" });
+db.SaveChanges();
+Console.WriteLine("將資料讀回");
+var blogs = db.Blogs!.OrderBy(b => b.BlogId);
+foreach (var b in blogs)
+{
+    Console.WriteLine($"{b.BlogId},{b.Uri}");
+}
